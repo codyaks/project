d15834f4d66746936fdb4c50f0d5dc32544e9f83
codyaks <parulphy1@gmail.com>
2025-05-22 11:47:31 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,26 @@
+a=5
+print(type(a))
+b=2.45
+print(type(b))
+c="hello"
+print(type(c))
+d=False
+print(type(d))
+name="akshaj"
+print(name)
+print(type(name))
+age=str(15)
+print(age)
+print(type(age))
+weight=(40.3)
+weight=int(40.3)
+print(weight)
+print(type(weight))
+student=True
+student=str(student)
+print(student)
+print(type(student))
+text=input("enter your name-")
+revtext=text[::-1]
+print("reverse of the string value is -")
+print(revtext)
