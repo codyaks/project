dd32b9724f9fe133a4a092823f4123514d54edf3
codyaks <parulphy1@gmail.com>
2026-01-18 18:45:48 +0530

    Add files via upload

--- chatbot_nlp.py
+++ chatbot_nlp.py
@@ -5,9 +5,8 @@ init(autoreset=True)
 destinations = {"mountain":["Rocky Mountains", "Alps", "Himalayas"],
                 "beach":["Maldives", "Bali", "Hawaii"],
                 "city":["New York", "Paris", "Tokyo"]}
-jokes=["Why don't scientists trust atoms? Because they make up everything!",
-       "Why did the scarecrow win an award? Because he was outstanding in his field!",
-       "Why don't programmers like nature? It has too many bugs."]
+jokes1=["Why don't scientists trust atoms? Because they make up everything!",
+       "Why did the scarecrow win an award? Because he was outstanding in his field!","Why don't programmers like nature? It has too many bugs."]
 def normalize_input(text):
     return re.sub(r"\s+"," ", text.strip().lower())
 def recommend():
@@ -22,7 +21,7 @@ def recommend():
         ans=input(Fore.GREEN+"you:- ").lower()
 
         if ans=="yes":
-            ptint(Fore.CYAN+"bot:- Great! Have a wonderful trip!")
+            print(Fore.CYAN+"bot:- Great! Have a wonderful trip!")
         elif ans=="no":
             print(Fore.CYAN+"bot:- No worries! lets try again.")
             recommend()
@@ -33,4 +32,36 @@ def recommend():
         print(Fore.CYAN+"bot:- Sorry, I don't have recommendations for that type.")
 
         show_help()
-        
+def jokes():
+    print(Fore.CYAN+f"travelbot:- Here's a joke for you: {random.choice(jokes1)}")
+    show_help()
+
+def show_help():
+    print(Fore.CYAN+"travelbot:- I can help you with the following commands:")
+    print(Fore.CYAN+"- I can give travel destination recommendations based on your preferences. type-(recommend)")
+    print(Fore.CYAN+"- I can tell you a joke to lighten your mood. type-(joke)")
+    print(Fore.CYAN+"to exit type exit")
+
+def chat():
+    print(Fore.CYAN+"Hi! I'am travelbot. Here to help you")
+    name=input(Fore.CYAN+"travelbot:- What's your name? \n"+Fore.GREEN+"you:- ")
+    print(Fore.CYAN+f"travelbot:- Nice to meet you, {name}!")
+    show_help()
+
+    while True:
+        user_input=input(Fore.GREEN+"you:- ")
+        user_input=normalize_input(user_input)
+
+        if user_input==("recommend"):
+            recommend()
+        elif user_input==("joke"):
+            jokes()
+        elif user_input==("exit"):
+            print(Fore.CYAN+"travelbot:- Goodbye! Safe travels!")
+            break
+        else:
+            print(Fore.CYAN+"travelbot:- I'm sorry, I didn't understand that command.")
+            
+
+if __name__ == "__main__":
+    chat()
